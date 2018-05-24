3169d0ac778591b2aad5160b20236d0592e2f77b
Blust96 <q.bohain@gmail.com>
2018-05-25 00:26:17 +0200

    Gitignore update

--- config/database.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    username: 'qbohain',
-    password: 'qbohain',
-    getUrl: (username, password) => {
-        return 'mongodb+srv://' + username + ':' + password + '@fortniteapi-6njq0.mongodb.net/fortniteDatas?retryWrites=true';
-    }
-};
